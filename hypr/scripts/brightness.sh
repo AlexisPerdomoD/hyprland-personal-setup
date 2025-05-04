e673ad66cea6ddff21fcc16f6eaec80ac23d13b4
alexisperdomod <dropdibemol@gmail.com>
2025-05-03 19:08:18 -0500

    minur updates

---
+++ hypr/scripts/brightness.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+STEP=5
+VCP=10    # código VCP para brillo
+MONITOR=1 # número del monitor si tienes más de uno
+
+current=$(ddcutil getvcp $VCP --brief --display $MONITOR | awk '{print $4}')
+action=$1
+
+case "$action" in
+up)
+    new=$((current + STEP))
+    ;;
+down)
+    new=$((current - STEP))
+    ;;
+*)
+    echo "Uso: $0 up | down"
+    exit 1
+    ;;
+esac
+
+# limita entre 0 y 100
+new=$(($new < 0 ? 0 : ($new > 100 ? 100 : $new)))
+
+sudo ddcutil setvcp $VCP $new --display $MONITOR
+echo "Brillo ajustado a $new"
