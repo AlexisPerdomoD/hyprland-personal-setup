d0f8de802cd44e1c47654479117257660d6265a8
alexisperdomod <dropdibemol@gmail.com>
2025-05-10 15:19:53 -0500

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 sudo pacman -S --needed --noconfirm hyprland hyprpaper hyprshot wlogout mako waybar wofi alacritty gtk-3.0 gtk-4.0 slurp wf-recorder zellij
 
+rm -rf ~/.config/hypr
+rm -rf ~/.config/mako
+rm -rf ~/.config/waybar
+rm -rf ~/.config/wofi
+rm -rf ~/.config/zellij
+
 ln -s ./hypr ~/.config/hypr
 ln -s ./mako ~/.config/mako
 ln -s ./waybar ~/.config/waybar
