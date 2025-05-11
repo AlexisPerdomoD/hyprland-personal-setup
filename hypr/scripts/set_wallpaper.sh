25fcc5b68665a7f7fea0c9611e085b788310339c
alexisperdomod <dropdibemol@gmail.com>
2025-05-11 00:54:16 -0500

    update

--- hypr/scripts/set_wallpaper.sh
+++ hypr/scripts/set_wallpaper.sh
@@ -13,8 +13,8 @@ done
 
 WALLPAPER_DIR="$HOME/Documents/Wallpapers/"
 HISTORY_FILE="$HOME/Documents/.wallpaper_history"
-next_index=0
-last_wallpaper=""
+NEXT_WALLPAPER_POSITION=0
+LAST_WALLPAPER=""
 WALLPAPERS=($(find "$WALLPAPER_DIR" -type f | sort))
 
 if [[ ${#WALLPAPERS[@]} -eq 0 ]]; then
@@ -23,39 +23,37 @@ if [[ ${#WALLPAPERS[@]} -eq 0 ]]; then
 fi
 
 if [[ -f $HISTORY_FILE ]]; then
-    last_wallpaper=$(cat $HISTORY_FILE)
+    LAST_WALLPAPER=$(cat $HISTORY_FILE)
 fi
 
-if [[ $last_wallpaper == "" ]]; then
-    next_index=1
+if [[ $LAST_WALLPAPER == "" ]]; then
+    NEXT_WALLPAPER_POSITION= 1
 else
     for i in {1..$#WALLPAPERS[@]}; do
 
-        if [[ ${WALLPAPERS[$i]} == $last_wallpaper ]]; then
-            new_value=$((i + 1))
-            if [[ $new_value -gt $#WALLPAPERS ]]; then
-                next_index=1
-                break
+        if [[ ${WALLPAPERS[$i]} == $LAST_WALLPAPER ]]; then
+            NEXT_WALLPAPER_POSITION=$((i + 1))
+
+            if [[ $NEXT_WALLPAPER_POSITION -gt $#WALLPAPERS ]]; then
+                NEXT_WALLPAPER_POSITION= 1
             fi
 
-            next_index=$((new_value))
             break
         fi
     done
 fi
 
-if [[ $next_index == 0 ]]; then
-    next_index=1
+if [[ $NEXT_WALLPAPER_POSITION == 0 ]]; then
+    NEXT_WALLPAPER_POSITION=1
 fi
 
-WALLPAPER="${WALLPAPERS[$next_index]}"
+WALLPAPER="${WALLPAPERS[$NEXT_WALLPAPER_POSITION]}"
 echo "$WALLPAPER" >"$HISTORY_FILE"
+
 hyprctl hyprpaper preload $WALLPAPER &&
-    hyprctl hyprpaper wallpaper " ,$WALLPAPER"
+    hyprctl hyprpaper wallpaper " ,$WALLPAPER" &&
+    wal -i $WALLPAPER &&
+    ln -sf "$HOME/.cache/wal/colors-waybar.css" "$HOME/.config/waybar/colors.css" &&
+    ln -sf "$HOME/.cache/wal/colors-waybar.css" "$HOME/.config/wofi/colors.css"
 
-wal -i $WALLPAPER
-ln -sf "$HOME/.cache/wal/colors-waybar.css" "$HOME/.config/waybar/colors.css"
-ln -sf "$HOME/.cache/wal/colors-waybar.css" "$HOME/.config/wofi/colors.css"
-
-# Uncomment below if using `swww` for smooth transitions
-# swww img "$WALLPAPER" --transition-type grow --transition-duration 1
+# Unomment below if using `swww` for smooth transitions
