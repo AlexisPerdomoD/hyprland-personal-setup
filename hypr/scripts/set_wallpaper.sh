f39292ccc6e6d5081ee432f0305d50d605505a55
alexisperdomod <dropdibemol@gmail.com>
2025-05-10 20:34:31 -0500

    update

--- hypr/scripts/set_wallpaper.sh
+++ hypr/scripts/set_wallpaper.sh
@@ -1,21 +1,28 @@
 #!/bin/env zsh
 
-# Directory containing wallpapers
+local timeout=30
+while ! pgrep -x hyprpaper >/dev/null; do
+    if [[ $timeout -eq 0 ]]; then
+        notify-send "hyprpaper not running"
+        exit 1
+    fi
+    timeout=$((timeout - 1))
+    sleep 0.1
+done
+
 local WALLPAPER_DIR="$HOME/Documents/Wallpapers/"
 local HISTORY_FILE="$HOME/Documents/.wallpaper_history"
 local next_index=0
 local last_wallpaper=""
-
-# Get the list of wallpapers sorted alphabetically
 local WALLPAPERS=($(find "$WALLPAPER_DIR" -type f | sort))
 
 if [[ ${#WALLPAPERS[@]} -eq 0 ]]; then
-    echo "No wallpapers found in $WALLPAPER_DIR"
+    notify-send "No wallpapers found in $WALLPAPER_DIR"
     exit 1
 fi
 
-if [[ -f "$HISTORY_FILE" ]]; then
-    last_wallpaper=$(cat "$HISTORY_FILE")
+if [[ -f $HISTORY_FILE ]]; then
+    last_wallpaper=$(cat $HISTORY_FILE)
 fi
 
 if [[ $last_wallpaper == "" ]]; then
@@ -23,7 +30,7 @@ if [[ $last_wallpaper == "" ]]; then
 else
     for i in {1..$#WALLPAPERS[@]}; do
 
-        if [[ "${WALLPAPERS[$i]}" == "$last_wallpaper" ]]; then
+        if [[ ${WALLPAPERS[$i]} == $last_wallpaper ]]; then
             local new_value=$((i + 1))
             if [[ $new_value -gt $#WALLPAPERS ]]; then
                 next_index=1
